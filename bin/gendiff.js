edb977229bbcef6575c934d780d1e439545ab400
MussonTMN <mussontmn@gmail.com>
2022-11-15 16:59:44 +0300

    end third step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import { readFileSync } from 'node:fs';
+import gendiff from '../src/getDifference.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.2')
+  .version(JSON.parse(readFileSync('package.json')).version)
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
-
-program.parse();
+  .argument('<filepath1> <filepath2>')
+  .action((file1, file2) => console.log(gendiff(file1, file2)))
+  .parse();
---
+++ src/getDifference.js
@@ -0,0 +1,32 @@
+import _ from 'lodash';
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+
+const gendiff = (filepath1, filepath2) => {
+  const current = process.cwd();
+  const data1 = JSON.parse(readFileSync(path.resolve(current, filepath1), 'utf-8'));
+  const data2 = JSON.parse(readFileSync(path.resolve(current, filepath2), 'utf-8'));
+
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const keys = _.sortBy(_.union(keys1, keys2));
+
+  const result = keys.reduce((acc, key) => {
+    if (!Object.hasOwn(data1, key)) {
+      acc[`+ ${key}`] = data2[key];
+    } else if (!Object.hasOwn(data2, key)) {
+      acc[`- ${key}`] = data1[key];
+    } else if (data1[key] !== data2[key]) {
+      acc[`- ${key}`] = data1[key];
+      acc[`+ ${key}`] = data2[key];
+    } else {
+      acc[`  ${key}`] = data1[key];
+    }
+    return acc;
+  }, {});
+  return JSON.stringify(result, null, ' ').replaceAll('"', '');
+};
+
+export default gendiff;
+
+console.log(gendiff('__fixtures__/file1.json', '__fixtures__/file2.json'));
