52f8c8f58352f2f086c4104b852cfde2bd3520f0
musson <mussontmn@gmail.com>
2022-12-12 15:28:59 +0300

    fix stylish

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,22 +1,29 @@
 import _ from 'lodash';
 
+const replacer = ' ';
+const spacesCount = 4;
+const getIndent = (depth) => replacer.repeat(depth * spacesCount - 2);
+
+const signs = {
+  added: '+ ',
+  deleted: '- ',
+  unchanged: '  ',
+};
+
 export default (data) => {
-  const replacer = ' ';
-  const spacesCount = 4;
   const iter = (curentValue, depth) => {
     if (!_.isPlainObject(curentValue)) {
       return `${curentValue}`;
     }
-    const getIndent = (num) => replacer.repeat(num * spacesCount - 2);
     const bracketIndent = replacer.repeat((depth - 1) * spacesCount);
     const lines = Object
       .entries(curentValue)
       .map(([key, val]) => {
         switch (val.type) {
           case 'added':
-            return `${getIndent(depth)}+ ${key}: ${iter(val.value, depth + 1)}`;
           case 'deleted':
-            return `${getIndent(depth)}- ${key}: ${iter(val.value, depth + 1)}`;
+          case 'unchanged':
+            return `${getIndent(depth)}${signs[val.type]}${key}: ${iter(val.value, depth + 1)}`;
           case 'changed':
             return `${getIndent(depth)}- ${key}: ${iter(val.value1, depth + 1)}\n${getIndent(depth)}+ ${key}: ${iter(val.value2, depth + 1)}`;
           case 'nested':
